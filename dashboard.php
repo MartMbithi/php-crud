75bcd400a73a049d6ed3679d44341311ec9caee6
MartMbithi <martdevelopers254@gmail.com>
2020-08-13 15:14:26 +0300

    Refactored Notifications

--- dashboard.php
+++ dashboard.php
@@ -158,93 +158,9 @@
 
                                 <div class="mt-container mx-auto">
                                     <div class="timeline-line">
-                                        
-                                        <div class="item-timeline timeline-new">
-                                            <div class="t-dot">
-                                                <div class="t-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg></div>
-                                            </div>
-                                            <div class="t-content">
-                                                <div class="t-uppercontent">
-                                                    <h5>Logs</h5>
-                                                    <span class="">27 Feb, 2020</span>
-                                                </div>
-                                                <p><span>Updated</span> Server Logs</p>
-                                                <div class="tags">
-                                                    <div class="badge badge-primary">Logs</div>
-                                                    <div class="badge badge-success">CPanel</div>
-                                                    <div class="badge badge-warning">Update</div>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="item-timeline timeline-new">
-                                            <div class="t-dot">
-                                                <div class="t-success"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></div>
-                                            </div>
-                                            <div class="t-content">
-                                                <div class="t-uppercontent">
-                                                    <h5>Mail</h5>
-                                                    <span class="">28 Feb, 2020</span>
-                                                </div>
-                                                <p>Send Mail to <a href="javascript:void(0);">HR</a> and <a href="javascript:void(0);">Admin</a></p>
-                                                <div class="tags">
-                                                    <div class="badge badge-primary">Admin</div>
-                                                    <div class="badge badge-success">HR</div>
-                                                    <div class="badge badge-warning">Mail</div>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="item-timeline timeline-new">
-                                            <div class="t-dot">
-                                                <div class="t-danger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg></div>
-                                            </div>
-                                            <div class="t-content">
-                                                <div class="t-uppercontent">
-                                                    <h5>Task Completed</h5>
-                                                    <span class="">01 Mar, 2020</span>
-                                                </div>
-                                                <p>Backup <span>Files EOD</span></p>
-                                                <div class="tags">
-                                                    <div class="badge badge-primary">Backup</div>
-                                                    <div class="badge badge-success">EOD</div>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="item-timeline timeline-new">
-                                            <div class="t-dot">
-                                                <div class="t-warning"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg></div>
-                                            </div>
-                                            <div class="t-content">
-                                                <div class="t-uppercontent">
-                                                    <h5>Collect Docs</h5>
-                                                    <span class="">10 Mar, 2020</span>
-                                                </div>
-                                                <p>Collected documents from <a href="javascript:void(0);">Sara</a></p>
-                                                <div class="tags">
-                                                    <div class="badge badge-success">Collect</div>
-                                                    <div class="badge badge-warning">Docs</div>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="item-timeline timeline-new">
-                                            <div class="t-dot">
-                                                <div class="t-dark"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-server"><rect x="2" y="2" width="20" height="8" rx="2" ry="2"></rect><rect x="2" y="14" width="20" height="8" rx="2" ry="2"></rect><line x1="6" y1="6" x2="6" y2="6"></line><line x1="6" y1="18" x2="6" y2="18"></line></svg></div>
-                                            </div>
-                                            <div class="t-content">
-                                                <div class="t-uppercontent">
-                                                    <h5>Reboot</h5>
-                                                    <span class="">06 Apr, 2020</span>
-                                                </div>
-                                                <p>Server rebooted successfully</p>
-                                                <div class="tags">
-                                                    <div class="badge badge-warning">Reboot</div>
-                                                    <div class="badge badge-primary">Server</div>
-                                                </div>
-                                            </div>
-                                        </div>                                      
+                                        <?php
+                                            require_once('partials/_notifications.php');
+                                        ?>                                                                        
                                     </div>                                    
                                 </div>
                             </div>
--- partials/_notifications.php
+++ partials/_notifications.php
@@ -5,29 +5,78 @@
     $res = $stmt->get_result();
     while($operation = $res->fetch_object())
     {
-        //default profile pic if logged in user has no profile picture
+        //Strip timestamp to DD-MM-YYYY H:M Formart
+        $date = date('d-M-Y g:i', strtotime($operation->created_at));
         if($operation->operation_type == 'Borrow')
         {
             echo 
             "
+                <div class='item-timeline timeline-new'>
+                    <div class='t-dot'>
+                        <div class='t-primary'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                    </div>
+                    <div class='t-content'>
+                        <div class='t-uppercontent'>
+                            <h5>Borrow Book</h5>
+                            <span class=''>$date</span>
+                        </div>
+                        <p>$row->operation_desc</p>
+                    </div>
+                </div>
             ";
         }
         else if($operation->operation_type =='Return')
         {
             echo 
             "
+                <div class='item-timeline timeline-new'>
+                    <div class='t-dot'>
+                        <div class='t-success'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                    </div>
+                    <div class='t-content'>
+                        <div class='t-uppercontent'>
+                            <h5>Return Book</h5>
+                            <span class=''>$date</span>
+                        </div>
+                        <p>$row->operation_desc</p>
+                    </div>
+                </div>
             ";
         }
         else if($operation->operation_type == 'Lost')
         {
             echo 
             "
+                <div class='item-timeline timeline-new'>
+                    <div class='t-dot'>
+                        <div class='t-danger'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                    </div>
+                    <div class='t-content'>
+                        <div class='t-uppercontent'>
+                            <h5>Lost Book</h5>
+                            <span class=''>$date</span>
+                        </div>
+                        <p>$row->operation_desc</p>
+                    </div>
+                </div>
             ";
         }        
         else if($operation->operation_type == 'Damanged')
         { 
             echo 
             "
+            <div class='item-timeline timeline-new'>
+                <div class='t-dot'>
+                    <div class='t-warning'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                </div>
+                <div class='t-content'>
+                    <div class='t-uppercontent'>
+                        <h5>Damanaged Book</h5>
+                        <span class=''>$date</span>
+                    </div>
+                    <p>$row->operation_desc</p>
+                </div>
+            </div>
             ";
 
         }
@@ -35,6 +84,18 @@
         {
             echo 
             "
+            <div class='item-timeline timeline-new'>
+                <div class='t-dot'>
+                    <div class='t-primary'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                </div>
+                <div class='t-content'>
+                    <div class='t-uppercontent'>
+                        <h5>Other</h5>
+                        <span class=''>$date</span>
+                    </div>
+                    <p>$row->operation_desc</p>
+                </div>
+            </div>
             ";
         }
 
