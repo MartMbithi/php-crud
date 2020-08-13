10eaee92ba14aafa4a259b746718b83db275ad76
MartMbithi <martdevelopers254@gmail.com>
2020-08-13 17:16:47 +0300

    Implemented View On Librarian

---
+++ view_librarian.php
@@ -0,0 +1,149 @@
+<?php
+    session_start();
+    include('config/config.php');
+    include('config/checklogin.php');
+    check_login();
+    require_once('partials/_head.php');
+?>
+<body>
+    <!--  BEGIN NAVBAR  -->
+    <?php
+        require_once('partials/_navbar.php');
+        $view = $_GET['view'];
+        $ret="SELECT * FROM  librarians WHERE librarian_id = '$view'"; 
+        $stmt= $mysqli->prepare($ret) ;
+        $stmt->execute();
+        $res=$stmt->get_result();
+        while($lib=$res->fetch_object())
+        {
+            if($lib->librarian_profile_picture == '')
+            {
+                $profilePic = "<img src='assets/img/librarian.jpg' class='img-thumbnail img-fluid'  alt='avatar'>";
+            }
+            else
+            {
+                $profilePic = "<img src='assets/img/$lib->librarian_profile_picture' class='img-thumbnail img-fluid' alt='avatar'>";
+            }
+    ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN NAVBAR  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Librarians</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Manage Librarians</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">View Librarian</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span><?php echo $lib->librarian_name;?></span></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <?php
+            require_once('partials/_sidebar.php');
+        ?>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <div class="row layout-spacing">
+
+                    <!-- Content -->
+                    <div class="col-xl-4 col-lg-6 col-md-5 col-sm-12 layout-top-spacing">
+
+                        <div class="user-profile layout-spacing">
+                            <div class="widget-content widget-content-area">
+                                <div class="d-flex justify-content-between">
+                                    <h3 class="">Profile</h3>
+                                    <a href="update_librarian.php?update=<?php echo $lib->librarian_id;?>" class="mt-2 edit-profile"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
+                                </div>
+                                <div class="text-center user-info">
+                                    <?php echo $profilePic;?>
+                                    <p class=""><?php echo $lib->librarian_name;?></p>
+                                </div>
+                                <div class="user-info-list">
+
+                                    <div class="">
+                                        <ul class="contacts-block list-unstyled">
+                                            <li class="contacts-block__item">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-coffee"><path d="M18 8h1a4 4 0 0 1 0 8h-1"></path><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path><line x1="6" y1="1" x2="6" y2="4"></line><line x1="10" y1="1" x2="10" y2="4"></line><line x1="14" y1="1" x2="14" y2="4"></line></svg>
+                                                 <?php echo $lib->librarian_number;?>
+                                            </li>
+                                            <li class="contacts-block__item">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M14 22h5a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-5"></path><polyline points="11 16 15 12 11 8"></polyline><line x1="15" y1="12" x2="3" y2="12"></line></svg>                                                 
+                                                <?php
+                                                    if($lib->librarian_account_status == 'Denied Login')
+                                                    {
+                                                        echo "<span class='badge outline-badge-danger'>$lib->librarian_account_status</span>";
+                                                    }
+                                                    else
+                                                    {
+                                                        echo "<span class='badge outline-badge-success'>$lib->librarian_account_status</span>";
+                                                    }
+                                                ?>
+                                            </li>
+                                            <li class="contacts-block__item">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+                                                <?php echo $lib->librarian_address;?>
+                                            </li>
+                                            <li class="contacts-block__item">
+                                                <a href="mailto:<?php echo $lib->librarian_email;?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                                                <?php echo $lib->librarian_email;?>
+                                            </a>
+                                            </li>
+                                            <li class="contacts-block__item">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
+                                                <?php echo $lib->librarian_phone_number;?>
+                                            </li>
+                                        </ul>
+                                    </div>                                    
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-xl-8 col-lg-6 col-md-7 col-sm-12 layout-top-spacing">
+                        <div class="bio layout-spacing ">
+                            <div class="widget-content widget-content-area">
+                                <h3 class="">Bio</h3>
+                                <p>
+                                    <?php
+                                        echo $lib->librarian_bio;
+                                    ?>
+                                </p>
+                            </div>                                
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <?php require_once('partials/_footer.php');?>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+    <?php require_once('partials/_scripts.php'); }?>    
+</body>
+
+</html>
