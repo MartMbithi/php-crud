44e5d3bc1b5fbe65968c63dbd7a459b529471d48
MartMbithi <martdevelopers254@gmail.com>
2020-08-14 16:01:15 +0300

    Successful Intergration of Ajax

--- ajax.php
+++ ajax.php
@@ -15,3 +15,19 @@ if(!empty($_POST["regNumber"]))
 <?php
 }
 }
+
+
+if(!empty($_POST["studentName"])) 
+{
+    $id=$_POST['studentName'];
+    $stmt = $DB_con->prepare("SELECT * FROM  students WHERE student_reg_number = :id");
+    $stmt->execute(array(':id' => $id));
+?>
+<?php
+    while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+{
+?>
+<?php echo htmlentities($row['student_id']); ?>
+<?php
+}
+}
