ff65543b0f9f339cd65883522cb4d3e8701844d3
MartMbithi <martdevelopers254@gmail.com>
2020-08-14 10:46:56 +0300

    Implemented Update On Book Categories

---
+++ update_book_category.php
@@ -0,0 +1,146 @@
+<?php
+    session_start();
+    include('config/config.php');
+    include('config/checklogin.php');
+    check_login();
+    
+        if(isset($_POST['Book_Category']))
+        {
+            //Prevent Posting Blank Values
+            if ( empty($_POST["category_code"]) || empty($_POST["category_name"]) || empty($_POST['category_description']) ) 
+            {
+                $err="Blank Values Not Accepted";
+            }
+            else
+            {  
+                $category_code = $_POST['category_code'];
+                $category_name = $_POST['category_name'];
+                $category_description = $_POST['category_description'];
+                $update = $_GET['update'];
+                     
+                //Insert Captured information to a database table
+                $postQuery="UPDATE book_categories SET category_code =?, category_name =?, category_description =? WHERE category_id =?";
+                $postStmt = $mysqli->prepare($postQuery);
+                //bind paramaters
+                $rc=$postStmt->bind_param('sssi', $category_code, $category_name, $category_description, $update);
+                $postStmt->execute();
+                //declare a varible which will be passed to alert function
+                if($postStmt)
+                {
+                    $success = "Category Updated" && header("refresh:1; url=manage_book_categories.php");
+                }
+                else 
+                {
+                    $err = "Please Try Again Or Try Later";
+                } 
+            }
+        }    
+            
+    
+    require_once('partials/_head.php');
+    require_once('config/code-generator.php');
+?>
+<body>
+    <!-- BEGIN LOADER -->
+    <div id="load_screen"> <div class="loader"> <div class="loader-content">
+        <div class="spinner-grow align-self-center"></div>
+    </div></div></div>
+    <!--  END LOADER -->
+
+    <?php
+        require_once('partials/_navbar.php');
+        $update = $_GET['update'];
+        $ret="SELECT * FROM  book_categories WHERE category_id = '$update'"; 
+        $stmt= $mysqli->prepare($ret);
+        $stmt->execute();
+        $res=$stmt->get_result();
+        while($cat=$res->fetch_object())
+        {
+            
+    ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN NAVBAR  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Book Categories</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Manage Book Categories</a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Update Book Category</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span><?php echo $cat->category_name;?></span></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <?php require_once('partials/_sidebar.php');?>
+        <!--  END SIDEBAR  -->
+        <!--  BEGIN CONTENT PART  -->
+        <div id="content" class="main-content">
+            <div class="container">
+                <div class="container">
+                    <br>
+                    <div class="row">
+                        <div id="flFormsGrid" class="col-lg-12 layout-spacing">
+                            <div class="statbox widget box box-shadow">
+                                <div class="widget-header">
+                                    <div class="row">
+                                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                                            <h4>Fill All Fields</h4>
+                                        </div>                                                                        
+                                    </div>
+                                </div>
+                                <div class="widget-content widget-content-area">
+                                    <form method="POST" enctype="multipart/form-data">
+                                        <div class="form-row mb-4">
+                                            <div class="form-group col-md-6">
+                                                <label for="inputEmail4">Category Name</label>
+                                                <input type="text" name="category_name" value="<?php echo $cat->category_name;?>" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="inputPassword4">Category Code</label>
+                                                <input type="text" name="category_code" value="<?php echo $cat->category_code;?>" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="form-row mb-4">
+                                            <div class="form-group col-md-12">
+                                                <label for="inputAddress">Category Description</label>
+                                                <textarea  name="category_description" rows="10" class="form-control"><?php echo $cat->category_description;?></textarea>
+                                            </div>
+                                        </div>
+                                      <button type="submit" name="Book_Category" class="btn btn-primary mt-3">Update Category</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+                require_once('partials/_footer.php');
+            ?>
+        </div>
+        <!--  END CONTENT PART  -->
+    </div>
+    <?php require_once('partials/_scripts.php'); }?>   
+</body>
+
+</html>
