295b8634dc8ad8561a691975a62441591e56194f
MartMbithi <martdevelopers254@gmail.com>
2020-08-15 12:49:44 +0300

    Refactored Log Ins Added Home Link

--- config/checklogin.php
+++ config/checklogin.php
@@ -5,7 +5,7 @@ if(strlen($_SESSION['login_id'])==0)
 	{
 		$host = $_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra="index.php";
+		$extra="login.php";
 		$_SESSION["login_id"]="";
 		header("Location: http://$host$uri/$extra");
 	}
