0b58887f5b7e82fc211d596d1e265157133bc369
MartMbithi <martdevelopers254@gmail.com>
2020-08-15 15:03:54 +0300

    Updated Notifications

--- partials/_notifications.php
+++ partials/_notifications.php
@@ -17,8 +17,11 @@
                     </div>
                     <div class='t-content'>
                         <div class='t-uppercontent'>
-                            <h5>Borrow Book</h5>
-                            <span class=''>$date</span>
+                            <h6>$operation_type</h6>
+                        </div>
+                        $operation->book_title <br> $operation->book_isbn_no
+                        <div class='tags'>
+                            <div class='badge badge-primary'>$date</div>
                         </div>
                     </div>
                 </div>
@@ -34,8 +37,11 @@
                     </div>
                     <div class='t-content'>
                         <div class='t-uppercontent'>
-                            <h5>Return Book</h5>
-                            <span class=''>$date</span>
+                        <h6>$operation->operation_type</h6>
+                        </div>
+                        $operation->book_title <br> $operation->book_isbn_no
+                        <div class='tags'>
+                            <div class='badge badge-success'>$date</div>
                         </div>
                     </div>
                 </div>
@@ -47,12 +53,17 @@
             "
                 <div class='item-timeline timeline-new'>
                     <div class='t-dot'>
-                        <div class='t-danger'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                        <div class='t-danger'>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-activity'><path d='M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z'></path><line x1='12' y1='9' x2='12' y2='13'></line><line x1='12' y1='17' x2='12' y2='17'></line></svg>                         
+                        </div>
                     </div>
                     <div class='t-content'>
                         <div class='t-uppercontent'>
-                            <h5>Lost Book</h5>
-                            <span class=''>$date</span>
+                            <h6>$operation->operation_type</h6>
+                        </div>
+                        $operation->book_title <br> $operation->book_isbn_no
+                        <div class='tags'>
+                            <div class='badge badge-danger'>$date</div>
                         </div>
                     </div>
                 </div>
@@ -64,12 +75,17 @@
             "
             <div class='item-timeline timeline-new'>
                 <div class='t-dot'>
-                    <div class='t-warning'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check'><polyline points='20 6 9 17 4 12'></polyline></svg></div>
+                    <div class='t-warning'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-activity'><circle cx='12' cy='12' r='10'></circle><line x1='12' y1='8' x2='12' y2='12'></line><line x1='12' y1='16' x2='12' y2='16'></line></svg>                     
+                    </div>
                 </div>
                 <div class='t-content'>
                     <div class='t-uppercontent'>
-                        <h5>Damanaged Book</h5>
-                        <span class=''>$date</span>
+                        <h6>$operation->operation_type</h6>
+                    </div>
+                    $operation->book_title <br> $operation->book_isbn_no
+                    <div class='tags'>
+                        <div class='badge badge-warning'>$date</div>
                     </div>
                 </div>
             </div>
@@ -86,8 +102,11 @@
                 </div>
                 <div class='t-content'>
                     <div class='t-uppercontent'>
-                        <h5>Other</h5>
-                        <span class=''>$date</span>
+                    <h6>$operation->operation_type</h6>
+                    </div>
+                    $operation->book_title <br> $operation->book_isbn_no
+                    <div class='tags'>
+                        <div class='badge badge-primary'>$date</div>
                     </div>
                 </div>
             </div>
