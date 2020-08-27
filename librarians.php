f5706dca839bc9834cea7b47d74fb89f17b27014
MartMbithi <martdevelopers254@gmail.com>
2020-08-27 03:20:51 +0300

    Refactored Advanced Reporting Added Export Functions

--- librarians.php
+++ librarians.php
@@ -1,24 +1,47 @@
 <?php
-    session_start();
-    include('config/config.php');
-    include('config/checklogin.php');
-    check_login();
-    
-    require_once('partials/_head.php');
-    
+session_start();
+include('config/config.php');
+include('config/checklogin.php');
+check_login();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Library Management System - Librarians Reports</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL CUSTOM STYLES -->
+    <link rel="stylesheet" type="text/css" href="plugins/table/datatable/datatables.css">
+    <link rel="stylesheet" type="text/css" href="plugins/table/datatable/custom_dt_html5.css">
+    <link rel="stylesheet" type="text/css" href="plugins/table/datatable/dt-global_style.css">
+    <!-- END PAGE LEVEL CUSTOM STYLES -->
+</head>
+
 <body>
-    
+
     <!--  BEGIN NAVBAR  -->
     <?php
-        require_once('partials/_navbar.php');
+    require_once('partials/_navbar.php');
     ?>
     <!--  END NAVBAR  -->
 
     <!--  BEGIN NAVBAR  -->
     <div class="sub-header-container">
         <header class="header navbar navbar-expand-sm">
-            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg></a>
             <ul class="navbar-nav flex-row">
                 <li>
                     <div class="page-header">
@@ -44,54 +67,57 @@
         <div class="search-overlay"></div>
 
         <!--  BEGIN SIDEBAR  -->
-        <?php 
-            require_once('partials/_sidebar.php');?>
+        <?php
+        require_once('partials/_sidebar.php');
         ?>
         <!--  END SIDEBAR  -->
 
         <!--  BEGIN CONTENT AREA  -->
         <div id="content" class="main-content">
             <div class="layout-px-spacing">
+
                 <div class="row layout-top-spacing">
+
                     <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
                         <div class="widget-content widget-content-area br-6">
                             <div class="table-responsive mb-4 mt-4">
                                 <table id="html5-extension" class="table table-hover non-hover" style="width:100%">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Number</th>
-                                            <th>Email</th>
-                                            <th>Phone</th>
-                                            <th>Address</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                            //Get all librarians
-                                            $ret="SELECT * FROM  librarians"; 
-                                            $stmt= $mysqli->prepare($ret) ;
-                                            $stmt->execute();
-                                            $res=$stmt->get_result();
-                                            while($lib=$res->fetch_object())
-                                            {
-
-                                        ?>
+                                    <table id="html5-extension" class="table table-hover non-hover" style="width:100%">
+                                        <thead>
                                             <tr>
-                                                <td>
-                                                    <span class="badge outline-badge-success">
-                                                        <a href="view_librarian.php?view=<?php echo $lib->librarian_id;?>">
-                                                            <?php echo $lib->librarian_number;?>
-                                                        </a>
-                                                    </span>
-                                                </td>
-                                                <td><?php echo $lib->librarian_name;?></td>
-                                                <td><?php echo $lib->librarian_email;?></td>
-                                                <td><?php echo $lib->librarian_phone_number;?></td>
-                                                <td><?php echo $lib->librarian_address;?></td>
+                                                <th>Name</th>
+                                                <th>Number</th>
+                                                <th>Email</th>
+                                                <th>Phone</th>
+                                                <th>Address</th>
                                             </tr>
-                                        <?php }?>
-                                    </tbody>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            //Get all librarians
+                                            $ret = "SELECT * FROM  librarians";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute();
+                                            $res = $stmt->get_result();
+                                            while ($lib = $res->fetch_object()) {
+
+                                            ?>
+                                                <tr>
+                                                    <td>
+                                                        <span class="badge outline-badge-success">
+                                                            <a href="view_librarian.php?view=<?php echo $lib->librarian_id; ?>">
+                                                                <?php echo $lib->librarian_number; ?>
+                                                            </a>
+                                                        </span>
+                                                    </td>
+                                                    <td><?php echo $lib->librarian_name; ?></td>
+                                                    <td><?php echo $lib->librarian_email; ?></td>
+                                                    <td><?php echo $lib->librarian_phone_number; ?></td>
+                                                    <td><?php echo $lib->librarian_address; ?></td>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
                                 </table>
                             </div>
                         </div>
@@ -100,12 +126,75 @@
                 </div>
 
             </div>
-            <?php require_once('partials/_footer.php');?>
+            <?php require_once('partials/_footer.php'); ?>
+
         </div>
         <!--  END CONTENT AREA  -->
     </div>
     <!-- END MAIN CONTAINER -->
-    <?php require_once('partials/_scripts.php');?>    
+
+
+
+    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="assets/js/libs/jquery-3.1.1.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/app.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            App.init();
+        });
+    </script>
+    <script src="assets/js/custom.js"></script>
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+
+    <!-- BEGIN PAGE LEVEL CUSTOM SCRIPTS -->
+    <script src="plugins/table/datatable/datatables.js"></script>
+    <!-- NOTE TO Use Copy CSV Excel PDF Print Options You Must Include These Files  -->
+    <script src="plugins/table/datatable/button-ext/dataTables.buttons.min.js"></script>
+    <script src="plugins/table/datatable/button-ext/jszip.min.js"></script>
+    <script src="plugins/table/datatable/button-ext/buttons.html5.min.js"></script>
+    <script src="plugins/table/datatable/button-ext/buttons.print.min.js"></script>
+    <script>
+        $('#html5-extension').DataTable({
+            dom: '<"row"<"col-md-12"<"row"<"col-md-6"B><"col-md-6"f> > ><"col-md-12"rt> <"col-md-12"<"row"<"col-md-5"i><"col-md-7"p>>> >',
+            buttons: {
+                buttons: [{
+                        extend: 'copy',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'csv',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'excel',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'print',
+                        className: 'btn'
+                    }
+                ]
+            },
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
+                },
+                "sInfo": "Showing page _PAGE_ of _PAGES_",
+                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+                "sSearchPlaceholder": "Search...",
+                "sLengthMenu": "Results :  _MENU_",
+            },
+            "stripeClasses": [],
+            "lengthMenu": [7, 10, 20, 50],
+            "pageLength": 7
+        });
+    </script>
+    <!-- END PAGE LEVEL CUSTOM SCRIPTS -->
 </body>
 
 </html>
